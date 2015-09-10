01c279f14d61f2a765b86a85b4add7660e979f6e
KrTaras <krtaras@gmail.com>
2015-09-10 18:54:41 +0300

    added auth logic

--- extension/app/js/vkAuthController.js
+++ extension/app/js/vkAuthController.js
@@ -1,6 +1,7 @@
 var VKAuthController = function () {
 	
 	var access_token = "";
+	var current_user = "";
 	
 	this.connect = function () {
 		var clientId = "client_id=5064446";
@@ -13,19 +14,40 @@ var VKAuthController = function () {
 				if (tabId == authTabId && changeInfo.url != undefined && changeInfo.status == "loading") {
 					if (changeInfo.url.indexOf('oauth.vk.com/blank.html') > -1) {
 						console.log(changeInfo.url);
-						console.log(getTokenFromURL(changeInfo.url));
+						access_token = getTokenFromURL(changeInfo.url);
+						current_user = getUserId(changeInfo.url);
+						console.log(access_token);
+						console.log(current_user);
 						chrome.tabs.remove(tabId, function () { });
+						getUserImgURL();
 					}
 				}
 			});
 		});
-	}
+	};
+	
+	var getUserImgURL = function() {
+		var url = "https://api.vk.com/method/users.get?user_id="+ current_user + "&v=5.37&access_token=" + access_token;
+		$.get(url, function(data){
+			console.log(data);
+		}, "json");
+	};
 	
 	var getTokenFromURL = function(url) {
-		var startIndex = url.indexOf("access_token=");
-		var endIndex = url.indexOf("expires_in");
-		url = url.substring(startIndex, endIndex);
-		url = url.replace("access_token=","");
-		return url;
-	}
+		var tmpUrl = url;
+		var startIndex = tmpUrl.indexOf("access_token=");
+		var endIndex = tmpUrl.indexOf("&expires_in");
+		tmpUrl = tmpUrl.substring(startIndex, endIndex);
+		tmpUrl = tmpUrl.replace("access_token=","");
+		return tmpUrl;
+	};
+	
+	var getUserId = function(url) {
+		var tmpUrl = url;
+		var startIndex = tmpUrl.indexOf("user_id=");
+		var endIndex = tmpUrl.length;
+		tmpUrl = tmpUrl.substring(startIndex, endIndex);
+		tmpUrl = tmpUrl.replace("user_id=","");
+		return tmpUrl;
+	};
 }
