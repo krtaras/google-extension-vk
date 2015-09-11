65720f6a161c59267aa9c87b40a9b0df267443aa
KrTaras <krtaras@gmail.com>
2015-09-11 19:41:09 +0300

    update buttons

--- extension/app/js/controllers/userProfileController.js
+++ extension/app/js/controllers/userProfileController.js
@@ -7,8 +7,20 @@
         function UserProfileController($scope) {
             $scope.userProfilePhoto = background.BackgroundService.getMyImgUrl();
             $scope.includeURL = "/app/view/empty.html";
-            $scope.openMyAudio = function() {
-                $scope.includeURL = "/app/view/audioList.html";
+            $scope.openMyInfo = function() {
+                $scope.includeURL = "/app/view/empty.html";
+            }
+            $scope.openMyMessages = function() {
+                $scope.includeURL = "/app/view/messages.html";
+            }
+            $scope.openMyAudios = function() {
+                $scope.includeURL = "/app/view/audios.html";
+            }
+             $scope.openMyNews = function() {
+                $scope.includeURL = "/app/view/news.html";
+            }
+            $scope.openMySettings = function() {
+                $scope.includeURL = "/app/view/settings.html";
             }
         }
     ]);
